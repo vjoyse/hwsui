76f16e52528fb0929864965f9d36209afba35430
Joyse Vargas <joyse.vargas@fiserv.com>
2024-03-08 10:54:16 -0600

    [iOS] Included Chapter 9 - Drawing Project

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Joyse Vargas on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
